0d056ad8ce351c5d00146667f18d9893bd0f9c4b
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2021-02-01 14:34:36 -0500

    Restructuring #3. Removing MATLAB files.

--- lib/matlab/L_AQ.m
+++
@@ -1,9 +0,0 @@
-function [ res ] = L_AQ(A,Q,P)
-	%Description:
-	%	Assume that the initial estimation error covariance is P.
-	%	Update the estimation error covariance according to the linear equation:
-	%		L_AQ(P) = A P A' + Q
-
-	res = A*P*A'+Q;
-
-end
